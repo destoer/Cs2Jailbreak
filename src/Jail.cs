10bd382a480c26e43ed1bca8601353e3a2fccefb
MSWS <imodmaker@gmail.com>
2024-01-22 20:42:26 -0800

    Undo version change

--- src/Jail.cs
+++ src/Jail.cs
@@ -204,7 +204,7 @@ public class JailPlugin : BasePlugin, IPluginConfig<JailConfig>
 
     public override string ModuleName => "CS2 Jailbreak - destoer";
 
-    public override string ModuleVersion => "v1.2.2";
+    public override string ModuleVersion => "v0.3.3";
 
     public override void Load(bool hotReload)
     {
