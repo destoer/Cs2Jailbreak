7265184681b82cb013861480ad5c0050663e3aa5
destoer <destoer1234@protonmail.com>
2024-03-31 16:45:51 +0100

    move to dotnet 8

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-yes | cp src/bin/Debug/net7.0/* "../../plugins/Cs2Jailbreak/"
-yes | cp WardenService/bin/Debug/net7.0/* "../../shared/WardenService/"
+yes | cp src/bin/Debug/net8.0/* "../../plugins/Cs2Jailbreak/"
+#yes | cp WardenService/bin/Debug/net8.0/* "../../shared/WardenService/"
 cp -r lang "../../plugins/Cs2Jailbreak/"
